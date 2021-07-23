a430d7b59283f20ecaa59034ef62bb0fdf670b29
Yonggang Yu <yonggangyu@ucar.edu>
2021-07-23 10:49:25 -0600

    update p1.sh

--- routine_test_setup/bundle_p1.sh
+++ routine_test_setup/bundle_p1.sh
@@ -11,7 +11,7 @@ REL_DIR=/glade/scratch/$user/$name_jedi_dir
 CODE_DIR=code    # Changing this will require changes to the automated cycling scripts.
 BUILD_DIR=build  # Changing this will require changes to the automated cycling scripts.
 echo "src_build_run_dir =$REL_DIR"
-bunle_branch="develop"
+bundle_branch="develop"
 ecbuild_option="--build=RelWithDebInfo"
 # ecbuild_option="--build=RelWithDebInfo -DBUNDLE_SKIP_ECKIT=OFF  -DBUNDLE_SKIP_FCKIT=OFF  -DBUNDLE_SKIP_ATLAS=OFF"
 
@@ -19,16 +19,18 @@ ecbuild_option="--build=RelWithDebInfo"
 status=FAILURE
 body='Unexpected failure of mpas-bundle autotest script.'
 
+
 [[ -d $REL_DIR/$CODE_DIR  ]] && \
-   mv -f $REL_DIR/$CODE_DIR  $REL_DIR/${CODE_DIR}_$(date +"%Y%m%d%H%M%S")
+   mv -f $REL_DIR/$CODE_DIR  $REL_DIR/${CODE_DIR}_$(date '+%Y-%m-%d_%H.%M.%S')
 mkdir -p $REL_DIR/$CODE_DIR
 cd $REL_DIR/$CODE_DIR
-git clone -b ${bunle_branch} git@github.com:JCSDA-internal/mpas-bundle.git
+git clone -b ${bundle_branch} git@github.com:JCSDA-internal/mpas-bundle.git
 sed -i_HTTP 's/https:\/\/github.com\//git@github.com:/' mpas-bundle/CMakeLists.txt
 source $REL_DIR/$CODE_DIR/mpas-bundle/env-setup/gnu-openmpi-cheyenne.sh
 
+
 [[ -d $REL_DIR/$BUILD_DIR ]] && \
-   mv -f $REL_DIR/$BUILD_DIR $REL_DIR/${BUILD_DIR}_$(date +"%Y%m%d%H%M%S")
+   mv -f $REL_DIR/$BUILD_DIR $REL_DIR/${BUILD_DIR}_$(date '+%Y-%m-%d_%H.%M.%S')
 mkdir -p $REL_DIR/$BUILD_DIR
 cd $REL_DIR/$BUILD_DIR
 ecbuild  $ecbuild_option  $REL_DIR/$CODE_DIR/mpas-bundle
