3dbbd8602edbf984070fa5792867de1f47b162eb
Yonggang Yu <yonggangyu@ucar.edu>
2021-07-23 10:25:54 -0600

    Update bundle_p1.sh with $(date '+%Y-%m-%d_%H.%M.%S')

--- autotest-setup/bundle_p1.sh
+++ autotest-setup/bundle_p1.sh
@@ -3,7 +3,6 @@
 name_jedi_dir="mpasbundletest"
 [[ $# -ge 1 ]] && echo $1 && name_jedi_dir=$1   # override dirname, optional
 
-
 # Customize variables
 AUTOTEST_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 user=${USER}
@@ -12,9 +11,9 @@ REL_DIR=/glade/scratch/$user/$name_jedi_dir
 CODE_DIR=code    # Changing this will require changes to the automated cycling scripts.
 BUILD_DIR=build  # Changing this will require changes to the automated cycling scripts.
 echo "src_build_run_dir =$REL_DIR"
-bunle_branch="develop"
+bundle_branch="develop"     # default "develop"
 ecbuild_option="--build=RelWithDebInfo"
-
+# ecbuild_option="--build=RelWithDebInfo -DBUNDLE_SKIP_ECKIT=OFF  -DBUNDLE_SKIP_FCKIT=OFF  -DBUNDLE_SKIP_ATLAS=OFF"
 
 # Default email subject and body variables
 status=FAILURE
@@ -22,16 +21,16 @@ body='Unexpected failure of mpas-bundle autotest script.'
 
 
 [[ -d $REL_DIR/$CODE_DIR  ]] && \
-   mv -f $REL_DIR/$CODE_DIR  $REL_DIR/${CODE_DIR}$(date +"%Y%m%d%h%s")
+   mv -f $REL_DIR/$CODE_DIR  $REL_DIR/${CODE_DIR}_$(date '+%Y-%m-%d_%H.%M.%S')
 mkdir -p $REL_DIR/$CODE_DIR
 cd $REL_DIR/$CODE_DIR
-git clone -b ${bunle_branch} git@github.com:JCSDA-internal/mpas-bundle.git
+git clone -b ${bundle_branch} git@github.com:JCSDA-internal/mpas-bundle.git
 sed -i_HTTP 's/https:\/\/github.com\//git@github.com:/' mpas-bundle/CMakeLists.txt
 source $REL_DIR/$CODE_DIR/mpas-bundle/env-setup/gnu-openmpi-cheyenne.sh
 
 
 [[ -d $REL_DIR/$BUILD_DIR ]] && \
-   mv -f $REL_DIR/$BUILD_DIR $REL_DIR/${BUILD_DIR}_$(date +"%Y%m%d%h%s")
+   mv -f $REL_DIR/$BUILD_DIR $REL_DIR/${BUILD_DIR}_$(date '+%Y-%m-%d_%H.%M.%S')
 mkdir -p $REL_DIR/$BUILD_DIR
 cd $REL_DIR/$BUILD_DIR
 ecbuild  $ecbuild_option  $REL_DIR/$CODE_DIR/mpas-bundle
