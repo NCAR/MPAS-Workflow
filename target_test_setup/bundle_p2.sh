d2658ec9f72958b898a15bddf0fb412db3a3b051
Yonggang Yu <yonggangyu@ucar.edu>
2021-08-02 08:12:36 -0600

    .

--- target_test_setup/bundle_p2.sh
+++ target_test_setup/bundle_p2.sh
@@ -22,7 +22,7 @@ body='Unexpected failure of mpas-bundle autotest script.'
 source $REL_DIR/$CODE_DIR/mpas-bundle/env-setup/gnu-openmpi-mac.sh
 mkdir -p $REL_DIR/$BUILD_DIR
 cd $REL_DIR/$BUILD_DIR
-make -j12
+make -j16
 
 # Check if build was successful by checking for presence of final built executable
 if [[ -f "$REL_DIR/$BUILD_DIR/bin/mpasjedi_variational.x" ]]; then
