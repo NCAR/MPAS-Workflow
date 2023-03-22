e92e59e8af637744f23845eec0c5951bf3374ed4
jjguerrette <guerrett@ucar.edu>
2023-03-22 14:46:45 -0600

    Update build again

--- initialize/framework/Build.py
+++ initialize/framework/Build.py
@@ -16,11 +16,11 @@ class Build(Component):
   variablesWithDefaults = {
     ## mpas bundle
     # mpas-bundle build directory
-    'mpas bundle': ['/glade/work/guerrett/pandac/build/mpas-bundle_gnu-openmpi_14MAR2023_single', str],
+    'mpas bundle': ['/glade/work/guerrett/pandac/build/mpas-bundle_gnu-openmpi_22MAR2023_single', str],
 
 
     # optional double-precision build
-    #'mpas bundle': ['/glade/work/guerrett/pandac/build/mpas-bundle_gnu-openmpi_14MAR2023', str],
+    #'mpas bundle': ['/glade/work/guerrett/pandac/build/mpas-bundle_gnu-openmpi_22MAR2023', str],
 
     ## compiler used
     # {compiler}-{mpi-implementation}/{version} combination that selects the JEDI module used to build
