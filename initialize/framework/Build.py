6f625899493d019d397c9de43cbf91a15e9521de
ibanos90 <ibanos90@gmail.com>
2024-04-02 00:23:40 -0600

    Add Cloud_Seeding parameter, remove 'max frame size' and update build directory

--- initialize/framework/Build.py
+++ initialize/framework/Build.py
@@ -41,7 +41,7 @@ class Build(Component):
     system = os.getenv('NCAR_HOST')
     if system == 'derecho':
       self.variablesWithDefaults['mpas bundle'] = \
-          ['/glade/work/nystrom/Code/JEDI/jcsda_internal/mpas-bundle_develop_2-28-24/gnu_build_4dhybrid/', str]
+          ['/glade/campaign/mmm/parc/ivette/pandac/codeBuild/mpasBundle_dev_SP/mpas-bundle/build', str]
       self.variablesWithDefaults['bundle compiler used'] = ['gnu-cray', str,
         ['gnu-cray', 'intel-cray']]
       self.variablesWithDefaults['forecast directory'] = ['bundle', str]
