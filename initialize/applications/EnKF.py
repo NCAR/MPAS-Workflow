f6199a1bbe61224180bcfbb2c3b7c8ae210eca8a
Simon Toedtli <stoedtli@ucar.edu>
2025-11-21 14:06:33 -0700

    Introduce option to couple EnKF run to var DA run

--- initialize/applications/EnKF.py
+++ initialize/applications/EnKF.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from collections import OrderedDict
+from getpass import getuser
+import os.path
 
 from initialize.applications.Members import Members
 
@@ -86,6 +88,7 @@ class EnKF(Component):
     'biasCorrection': [False, bool],
 
     # directories that stores varBC coefficients that are updated with variational DA
+    # if coupledToVarDA == True the staticVarBcDir is automatically set to the coupled var DA directory
     'staticVarBcDir': ['/glade/campaign/mmm/parc/ivette/pandac/year7Exp/ivette_3dhybrid-allsky-60-60-iter_O30kmI60km_ensB-SE80+RTPP70_VarBC_v3.0.2_newBenchmark_allsky-amsua/CyclingDA', str],
 
     ## tropprsMethod
@@ -118,6 +121,34 @@ class EnKF(Component):
     ## IR/VIS land surface coefficients classification
     # OPTIONS: USGS, IGBP, NPOESS
     'IRVISlandCoeff': ['IGBP', str],
+
+    ## Coupling to an external variational DA run
+    # When coupledToVarDA is true, this EnKF run can be recentered (recenterAnalyses == True) on the variational DA run and/or
+    # it can use the variational bias correction coefficients (biasCorrection == True) of the variational DA run
+    'coupledToVarDA': [False, bool],
+
+    ## Recentering of analysis ensemble
+    # When recenterAnalyses is true, the analysis ensemble of this EnKF is recentered on the analysis of a
+    # concurrently running var DA run. Requires coupledToVarDA == True.
+    # The recentering application is not implemented yet.
+    'recenterAnalyses': [False, bool],
+  }
+
+  optionalVariables = {
+    ## Coupling to an external variational DA run
+    # Must be specified if coupledToVarDA == True, not used otherwise
+    # Specify the full name of the workflow, constructed as:
+    # Experiment['prefix'] + Experiment['name'] + Experiment['suffix'] + Experiment['suite identifier']
+    # The default values are:
+    # Experiment['prefix'] = "${USER}_"
+    # Experiment['name']: see Cycle.py
+    # Experiment['suffix'] = ""
+    # Experiment['suite identifier'] = ""
+    # It is best practice to specify the experiment prefix, name, and suffix in the coupled yaml files
+    # to make sure the coupled workflow can be identified correctly.
+    # Note: the workflow automatically prepends 'MPAS-Workflow' to workflowNameVarDA where needed
+    # to make the identifier consistent with submit.csh
+    'workflowNameVarDA': str,
   }
 
   def __init__(self,
@@ -182,6 +213,28 @@ class EnKF(Component):
 
     # TODO: this needs to be non-zero for EnKF workflows that use IAU, get value from forecast
     self._set('ensPbOffsetHR', 0)
+
+    # coupling to variational DA
+    if self['coupledToVarDA']:
+      # A coupled EnKF run should use some information from the variational DA.
+      # Stop the run if no information is used in the current configuration to make sure
+      # user can adjust settings.
+      if not self['biasCorrection'] and not self['recenterAnalyses']:
+        raise ValueError(("Coupled EnKF does not use any information from var DA."
+                          "biasCorrectionEnKF and/or recenterAnalyses should be set to true."))
+      # A coupled EnKF run requires the name of the variational DA workflow to
+      # set the external dependency
+      if self['workflowNameVarDA'] is None:
+        raise ValueError("workflowNameVarDA has to be specified for a coupled EnKF")
+      # Overwrite the static var BC directory to make sure we are reading the satbias files from
+      # the coupled var DA run
+      workDirVarDA = os.path.join('/glade', 'derecho', 'scratch', getuser(),
+                           'pandac', self['workflowNameVarDA'])
+      self._set('staticVarBcDir', f'{os.path.join(workDirVarDA, "CyclingDA")}')
+    else:
+      # Recentering the analysis ensemble is not defined if the EnKF run is not coupled.
+      if self['recenterAnalyses']:
+        raise NotImplementedError("Recentering of the analyses ensemble is undefined if the EnKF run is not coupled.")
 
     self._cshVars = list(self._vtable.keys())
 
@@ -262,6 +315,23 @@ class EnKF(Component):
     inherit = '''+self.tf.execute+''', BATCH
     script = $origin/bin/EnKF.csh Solver
 '''+solvertask.job()+solvertask.directives()]
+      
+      if self['coupledToVarDA']:
+        # The coupled EnKF run uses the analysis file and/or the variational bias correction files
+        # from the variational DA run. This introduces a dependency between the EnKF and the
+        # variational DA at the current cycle point. The following external trigger defines this dependency.
+        # Note: submit.csh prepends 'MPAS-Workflow/' to the workflow name to generate the workflow ID
+        workflowIdVarDA = os.path.join('MPAS-Workflow', self['workflowNameVarDA'])
+        callIntervalInS = 30  # call interval for external trigger (default is 10)
+        self._xtriggers +=[('\n'
+                            '    var_da = workflow_state('
+                            f'workflow="{workflowIdVarDA}", '
+                            'task="DAFinished__", point="%(point)s")'
+                            f':PT{callIntervalInS}S')]
+        
+        # Add the dependency to the graph
+        self._dependencies += [('\n'
+                                '        @var_da => ' + self.tf.pre)]
 
       if self['diagEnKFOMA'] and self['retainObsFeedback']:
          self._tasks += ['''
